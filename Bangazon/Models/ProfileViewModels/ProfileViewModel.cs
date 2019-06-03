eafb2b5848ae81fc31efba375abd291c05b6b57a
Brian Neal <brianbneal@gmail.com>
2019-06-03 11:37:35 -0500

    working on profile page view

---
+++ Bangazon/Models/ProfileViewModels/ProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProfileViewModels
+{
+    public class ProfileViewModel
+    {
+        //public ProfileViewModel(ApplicationUser user, List<PaymentType> paymentTypes)
+        //{
+        //    User = user;
+        //    PaymentTypes = paymentTypes;
+        //}
+        public ApplicationUser User { get; set; }
+
+        public IEnumerable<PaymentType> PaymentTypes { get; set; }
+
+    }
+}
